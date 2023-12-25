febd5642680b5ecdf6787757e6ad4f197234e46c
Dennis <denkipyator@gmail.com>
2023-12-25 14:55:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function findMatching(array,string){
+    let lowerCaseString = string.toLowerCase();
+    let newArray = [];
+    for(let person in array){
+        if(array[person] === lowerCaseString || array[person] === string){
+            newArray.push(array[person]);}
+        }
+    return newArray;
+}
+function fuzzyMatch(array,string){
+    let newArray = [];
+    for(let person in array){
+        if(array[person].startsWith(string)){
+            newArray.push(array[person])};
+        }
+    return newArray;  
+}
+function matchName(array,string){
+    let newArray = [];
+    for(let person of array){
+        if(person.name == string){
+            newArray.push(person);}
+        }
+    return newArray;
+}
 // Code your solution here
